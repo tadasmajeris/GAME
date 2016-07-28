f9e4f185e8748af7df06330779dc23eb11882a1d
Tadas Majeris <tadas.majeris@gmail.com>
2016-07-28 18:14:31 +0100

    Added flee option. Added stats display

--- battle.rb
+++ battle.rb
@@ -1,13 +1,15 @@
 class Hero
-  attr_reader   :strength, :health
+  attr_reader   :health, :strength, :stealth
   
-  def initialize health, strength
+  def initialize health, strength, stealth
     @health = health
     @strength = strength
+    @stealth = stealth
+    @fled = false
   end
   
   def print_rolls dice_rolls
-    print "\nRolls: "
+    print "Rolls: "
     
     dice_rolls.each { |die|
       print case die 
@@ -18,13 +20,28 @@ class Hero
     puts "\n"
   end
   
+  def flee(monster)
+    dice_rolls = []
+    stealth.times { dice_rolls << rand(1..6) }
+    print_rolls dice_rolls
+    successes = dice_rolls.count { |die| die >= 5 }
+    if successes >= monster.perception
+      @fled = true
+    else
+      @fled = false
+      puts "> Unlucky... Monster spotted you!"
+    end
+  end
+  
   def attack(monster)
+    puts "\nAttacking the monster"
     dice_rolls = []
     strength.times { dice_rolls << rand(1..6) }
     print_rolls dice_rolls
     
     successes = dice_rolls.count { |die| die >= 5 }
     if successes >= monster.toughness
+      puts "> Hit!!!!"
       return true
     else
       @health -= monster.damage
@@ -35,39 +52,56 @@ class Hero
   def dead?
     health <= 0
   end
-end
-
-class Monster
-  attr_reader   :damage, :toughness
   
-  def initialize toughness, damage
-    @toughness = toughness
-    @damage = damage
+  def fled?
+    @fled
   end
 end
 
+class Monster
+  attr_reader   :toughness, :damage, :perception
+  
+  def initialize toughness, damage, perception
+    @toughness = toughness
+    @damage = damage
+    @perception = perception
+  end
+end
 
-def print_special txt
-  puts '=' * 40
-  puts txt
+$LINE_WIDTH = 60
+
+def print_special txt=nil
+  puts '=' * $LINE_WIDTH
+  puts txt if txt
 end
 
 def battle_result(hero)
-  return hero.dead? ? "YOU LOST! :(" : "YOU WON! :)"
+  return "YOU LOST! :(" if hero.dead? 
+  return "YOU RAN AWAY..." if hero.fled?
+  "YOU WON!"
 end
 
 # PROGRAM
-hero = Hero.new 10, 5
-monster = Monster.new rand(2..3), 4
+hero = Hero.new 10, 5, 3
+monster = Monster.new rand(2..3), 4, 2
 
+print_special "BATTLE STARTS!"
+print "MONSTER: Toughness: #{monster.toughness}".ljust($LINE_WIDTH/3+5)
+print "Damage: #{monster.damage}".ljust($LINE_WIDTH/3)
+print "Perception: #{monster.perception}".ljust($LINE_WIDTH/3)
 puts
-print_special "BATTLE STARTED!"
-puts "Monster's toughness: #{monster.toughness}"
-puts "Hero's strength:     #{hero.strength}"
+print "HERO:    Strength: #{hero.strength}".ljust($LINE_WIDTH/3+5)
+print "Health: #{hero.health}".ljust($LINE_WIDTH/3)
+print "Stealth: #{hero.stealth}".ljust($LINE_WIDTH/3)
+puts
+print_special
 
-until hero.attack(monster) || hero.dead?
-  puts "You missed. Monster dealt #{monster.damage} damage."
+until hero.attack(monster) || hero.dead? || hero.fled?
+  puts "> You missed. Monster dealt #{monster.damage} damage."
   puts "Your current health is #{hero.health}"
+  
+  puts "Trying to flee..."
+  hero.flee(monster)
 end
 
 print_special battle_result(hero)+"\n\n"
