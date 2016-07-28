75439729b59b4f174120245f2f7c36d3c6f6d502
Tadas Majeris <tadas.majeris@gmail.com>
2016-07-28 22:34:38 +0100

    Fixed main battle program. Added printing tasks to GamePrinter class

--- battle.rb
+++ battle.rb
@@ -10,22 +10,17 @@ class Hero
   
   def flee(monster)
     dice = Dice.new stealth, monster.perception
-    if dice.roll_success?
-      @@fled = true
-    else
-      @@fled = false
-      puts "> Unlucky... Monster spotted you!"
-    end
+    @@fled = dice.roll_success?
   end
   
   def attack(monster)
-    puts "\nAttacking the monster"
     dice = Dice.new strength, monster.toughness
     if dice.roll_success?
-      puts "> Hit!!!!"
+      monster.toughness = 0
       return true
     else
       @health -= monster.damage
+      @health = 0 if health.negative?
       return false
     end
   end
@@ -40,12 +35,17 @@ class Hero
 end
 
 class Monster
-  attr_reader   :toughness, :damage, :perception
+  attr_accessor :toughness
+  attr_reader   :damage, :perception
   
   def initialize toughness, damage, perception
     @toughness = toughness
     @damage = damage
     @perception = perception
+  end
+  
+  def dead?
+    @toughness == 0
   end
 end
 
@@ -70,7 +70,7 @@ class Dice
     
     dice_rolls.each { |die|
       print case die 
-              when 1 then '⠂' when 2 then '⠈⠄' when 3 then '⠑⠄' 
+              when 1 then '⠂'  when 2 then '⠈⠄' when 3 then '⠑⠄' 
               when 4 then '⠨⠅' when 5 then '⠕⠅' when 6 then '⠸⠇' end
       print ' '
     }
@@ -78,45 +78,56 @@ class Dice
   end
 end
 
-$LINE_WIDTH = 60
+class GamePrinter
+  $LINE_WIDTH = 60
+  
+  def self.print_special txt=nil
+    puts '=' * $LINE_WIDTH
+    puts txt if txt
+  end
 
-def print_special txt=nil
-  puts '=' * $LINE_WIDTH
-  puts txt if txt
+  # prints object's name(class), it's variables and their values
+  def self.print_attributes object
+    print (object.class.to_s.upcase + ': ').rjust($LINE_WIDTH/4)
+    object.instance_variables.each { |attribute|
+      attribute = attribute[1..-1]
+      print (attribute + ":").ljust($LINE_WIDTH/4-4)
+      print "#{object.send(attribute)}  ".rjust(4)
+    }
+    puts
+  end
 end
 
+# --------------------
 def battle_result hero
   return "YOU LOST! :(" if hero.dead? 
   return "YOU RAN AWAY..." if hero.fled?
   "YOU WON!"
 end
 
-# prints object's name(class), it's variables and their values
-def print_attributes object
-  print (object.class.to_s.upcase + ': ').rjust($LINE_WIDTH/4)
-  object.instance_variables.each { |attribute|
-    attribute = attribute[1..-1]
-    print (attribute + ":").ljust($LINE_WIDTH/4-4)
-    print "#{object.send(attribute)}  ".rjust(4)
-  }
-  puts
-end
-
 # PROGRAM
 hero = Hero.new 10, 5, 3
 monster = Monster.new rand(2..3), 4, 2
 
-print_special "BATTLE STARTS!"
-print_attributes hero
-print_attributes monster
-print_special
+GamePrinter.print_special "BATTLE STARTS!"
+GamePrinter.print_attributes hero
+GamePrinter.print_attributes monster
+GamePrinter.print_special
 
-until hero.fled? || hero.attack(monster) || hero.dead?
-  puts "> You missed. Monster dealt #{monster.damage} damage."
-  puts "> Your current health is #{hero.health}"
+until hero.fled? || monster.dead? || hero.dead?
+  puts "\nAttacking the monster"
   
-  puts "Trying to flee..."
-  hero.flee(monster)
+  if hero.attack(monster)
+    puts "> Hit!!!!"
+  else
+    puts "> You missed. Monster dealt #{monster.damage} damage."
+    puts "> Your current health is #{hero.health}"
+    if !hero.dead?
+      puts "Trying to flee..."
+      hero.flee(monster)
+      puts "> Unlucky... Monster spotted you!" if !hero.fled?
+    end
+  end
 end
 
-print_special battle_result(hero)+"\n\n"
+GamePrinter.print_special battle_result(hero)+"\n\n"
