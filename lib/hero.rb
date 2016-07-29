e0d842f9d99df87f441cfc123a935f21f1481a31
Tadas Majeris <tadas.majeris@gmail.com>
2016-07-29 13:01:07 +0100

    Hero attacks monster

--- lib/hero.rb
+++ lib/hero.rb
@@ -4,5 +4,10 @@ class Hero
     def initialize attr={}
         @strength = attr[:strength] || 3
         @health   = attr[:health]   || 10
+        @dicepool = attr[:dicepool]
+    end
+    
+    def attack(monster)
+        @dicepool.skill_check(strength) >= monster.toughness
     end
 end
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require_relative '../../lib/hero'
 
 describe Hero do
+  let(:dicepool) { double("dicepool")  }
   
   describe "default attributes" do
     let(:hero) { Hero.new }
@@ -21,5 +22,20 @@ describe Hero do
   it "can be initialized with custom health" do
     hero = Hero.new health: 8
     expect(hero.health).to eq(8)
+  end
+  
+  describe "attack" do
+    it "succeeds" do
+      dicepool.stub(:skill_check).and_return(3)
+      hero = Hero.new dicepool: dicepool
+      monster = double("monster", toughness: 2)
+      expect(hero.attack(monster)).to be true
+    end
+    it "fails" do
+      dicepool.stub(:skill_check).and_return(1)
+      hero = Hero.new dicepool: dicepool
+      monster = double("monster", toughness: 2)
+      expect(hero.attack(monster)).to be false
+    end
   end
 end
